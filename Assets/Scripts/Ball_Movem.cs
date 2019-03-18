a43ea611bf175e9508aa11a3636b6bf83ab89744
CazNm <namaga132@gmail.com>
2019-03-18 23:22:12 +0900

    simple work

---
+++ Assets/Scripts/Ball_Movem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movem : MonoBehaviour
+{
+
+
+    
+    public Vector2 ball_force;
+    public int ball_colision;
+
+    Rigidbody2D ball_body;
+    Transform ball_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball_pos = GetComponent<Transform>();
+        ball_body = GetComponent<Rigidbody2D>();
+
+        ball_pos.position = Vector3.zero;
+        ball_body.AddForce(new Vector2(100.0f, 100.0f));
+
+        ball_colision = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
---
+++ Assets/Scripts/brick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brick : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        
+
+        if (col.gameObject != null)
+        {
+            GameObject col_object = col.gameObject;
+            
+            Vector3 Save_cal = GetComponent<Transform>().position - col_object.transform.position;
+
+            //x가 2.8이고 y가 
+            Debug.Log(Save_cal);
+
+
+        }
+    }
+
+}
